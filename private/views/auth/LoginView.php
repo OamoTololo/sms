a294658a77e42707d75799ce98431518b1215a08
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-11-07 19:49:18 +0200

    created and styled the register view page and modified the login page.

--- private/views/auth/LoginView.php
+++ private/views/auth/LoginView.php
@@ -3,7 +3,7 @@ $this->view('includes/header');
 ?>
     <title>Login</title>
     <div class="container-fluid">
-        <div class="p-5 mt-5 mx-auto shadow rounded" style="width: 100%; max-width: 340px;">
+        <div class="p-5 mt-5 mx-auto border border-dark shadow rounded" style="width: 100%; max-width: 340px;">
             <h2 class="text-center">SA Schools</h2>
             <img class="border border-primary d-block mx-auto img-fluid rounded-circle" style="width: 100px;"
                  src="assets/img/SA.png"
--- private/views/auth/RegisterView.php
+++ private/views/auth/RegisterView.php
@@ -3,25 +3,36 @@ $this->view('includes/header');
 ?>
     <title>Login</title>
     <div class="container-fluid">
-        <div class="p-5 mt-5 mx-auto shadow rounded" style="width: 100%; max-width: 340px;">
+        <div class="p-5 mt-5 mx-auto border border-dark shadow rounded" style="width: 100%; max-width: 340px;">
             <h2 class="text-center">SA Schools</h2>
-            <img class="d-block mx-auto img-fluid rounded-circle" style="width: 100px;" src="assets/img/SA.png" alt="Test Image">
-            <h3>Register</h3>
-            <input class="form-control" type="text" name="name" placeholder="Name" autocomplete="off">
+            <img class="d-block border border-primary mx-auto img-fluid rounded-circle" style="width: 100px;" src="assets/img/SA.png" alt="Test Image">
+            <h3>Register User</h3>
+            <input class="my-2 form-control" type="text" name="name" placeholder="Name" autocomplete="off">
+            <input class="my-2 form-control" type="text" name="surname" placeholder="Surname" autocomplete="off">
+            <input class="my-2 form-control" type="text" name="username" placeholder="Username" autocomplete="off">
+            <input class="my-2 form-control" type="date" name="date" placeholder="Email" autocomplete="off">
+            <input class="my-2 form-control" type="text" name="url_address" placeholder="URL Address" autocomplete="off">
+            <input class="my-2 form-control" type="text" name="gender" placeholder="Gender" autocomplete="off">
+            <input class="my-2 form-control" type="email" name="email" placeholder="Email" autocomplete="off">
+            <select class="my-2 form-control" name="gender" id="gender">
+                <option class="text-center" value="">Gender</option>
+                <option class="" value="">Female</option>
+                <option class="" value="">Male</option>
+                <option class="" value="">Other</option>
+            </select>
+            <select class="my-2 form-control" name="role" id="role">
+                <option class="text-center" value="">Role</option>
+                <option class="" value="student">Student</option>
+                <option class="" value="receptionist">Receptionist</option>
+                <option class="" value="lecturer">Lecturer</option>
+                <option class="" value="admin">Admin</option>
+                <option class="" value="super_admin">Super Admin</option>
+            </select>
+            <input class="my-2 form-control" type="password" name="password" placeholder="Password">
+            <input class="my-2 form-control" type="password" name="retyped_password" placeholder="Retype Password">
             <br>
-            <input class="form-control" type="text" name="surname" placeholder="Surname" autocomplete="off">
-            <br>
-            <input class="form-control" type="date" name="date" placeholder="Email" autocomplete="off">
-            <br>
-            <input class="form-control" type="text" name="url_address" placeholder="URL Address" autocomplete="off">
-            <br>
-            <input class="form-control" type="text" name="gender" placeholder="Gender" autocomplete="off">
-            <br>
-            <input class="form-control" type="email" name="email" placeholder="Email" autocomplete="off">
-            <br>
-            <input class="form-control" type="password" name="password" placeholder="Password">
-            <br>
-            <button class="btn btn-success">Login</button>
+            <button class="btn btn-success">Register</button>
+            <button class="btn btn-danger float-end text-white">Cancel</button>
         </div>
     </div>
 <?php
