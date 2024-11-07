f6ad5fcecb3a8f6254ec4370c57c2f2bc53a9ac2
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-11-07 19:17:49 +0200

    created dynamic headers and footer links in these files.

--- private/core/Controller.php
+++ private/core/Controller.php
@@ -12,9 +12,9 @@ class Controller
         extract($data);
 
         if (file_exists('../private/views/' . $view . '.php')) {
-            return file_get_contents('../private/views/' . $view . '.php');
+            require'../private/views/' . $view . '.php';
         } else {
-            return file_get_contents('../private/views/404.php');
+            require'../private/views/404.php';
         }
     }
 }
--- private/views/HomeView.php
+++ private/views/HomeView.php
@@ -1,13 +1,11 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-</head>
-<body>
-    <h1>This is home</h1>
-</body>
-</html>
+<?php
+$this->view('includes/header');
+?>
+
+<div class="container-fluid">
+    <i class="fa-solid fa-home"></i> This is home
+</div>
+
+<?php
+$this->view('includes/footer-links');
+?>
