8be74612f343a4b46a5591605b27ee3f8d9ee00b
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-11-08 00:24:18 +0200

    created breadcrumbs and included breadcrumbs in the profile script.

--- private/views/ProfileView.php
+++ private/views/ProfileView.php
@@ -1,7 +1,27 @@
 <?php
 $this->view('includes/Header');
+$this->view('includes/Navbar');
 ?>
+    <div class="container-fluid p-4 shadow mx-auto" style="max-width: 1000px;">
+        <?php $this->view('includes/BreadCrumb'); ?>
 
+        <div class="row">
+            <div class="col-4">
+                <img src="assets/img/SA.png" class="border border-danger d-block mx-auto img-fluid rounded-circle" style="width: 150px;">
+            </div>
+            <div class="col-8 bg-light p-2">
+                <table class="table table-hover table-striped table-bordered">
+                    <tr><th>Name:</th><td>Oamogetswe</td></tr>
+                    <tr><th>Surname:</th><td>Mgidi</td></tr>
+                    <tr><th>Gender:</th><td>Male</td></tr>
+                </table>
+            </div>
+        </div>
+
+        <div>
+
+        </div>
+    </div>
 <?php
 $this->view('includes/Footer-links');
 ?>
