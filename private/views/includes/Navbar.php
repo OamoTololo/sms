55e5793f3e9791f5e7f4eaf4154d6ee157a055d1
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-11-08 01:00:24 +0200

    fixed the route to the dashboard.

--- private/views/includes/Navbar.php
+++ private/views/includes/Navbar.php
@@ -10,7 +10,7 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Dashboard</a>
+                    <a class="nav-link active" aria-current="page" href="HomeController">Dashboard</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Classes</a>
