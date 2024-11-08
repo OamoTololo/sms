b1dc0f458d0651f7351a571f2f766f2adff1ab4a
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-11-08 18:36:51 +0200

    created logic to check if all the fields are filled with a validation check that redirects to login if passed.

--- private/controllers/RegisterController.php
+++ private/controllers/RegisterController.php
@@ -4,6 +4,18 @@ class RegisterController extends Controller
 {
     public function index()
     {
-        $this->view('auth/RegisterView', []);
+        $errors = [];
+        if (count($_POST) > 0) {
+            $user = new User;
+
+            if ($user->validate($_POST)) {
+                $this->redirect('auth/LoginView');
+            } else {
+                $errors = $user->errors;
+            }
+        }
+        $this->view('auth/RegisterView', [
+            'errors' => $errors
+        ]);
     }
 }
