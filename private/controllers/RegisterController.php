acf609ad8c41641a9a4340caee99125ed396b83c
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-11-09 00:44:11 +0200

    created logic to insert the fields into a table. Need to do a check if the user already exists.

--- private/controllers/RegisterController.php
+++ private/controllers/RegisterController.php
@@ -9,6 +9,19 @@ class RegisterController extends Controller
             $user = new User;
 
             if ($user->validate($_POST)) {
+                $data['name'] = $_POST['name'];
+                $data['surname'] = $_POST['surname'];
+                $data['username'] = $_POST['username'];
+                $data['date'] = date('Y-m-d H:i:s');
+                $data['url_address'] = $_POST['url_address'];
+                $data['email'] = $_POST['email'];
+                $data['gender'] = $_POST['gender'];
+                $data['role'] = $_POST['role'];
+                $data['school_id'] = $_POST['school_id'];
+                $data['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                $data['retyped_password'] = $_POST['retyped_password'];
+
+                $user->insert($data);
                 $this->redirect('/LoginController');
             } else {
                 $errors = $user->errors;
