1b0e58384b84e418adf649671d2c33bb568fd8a9
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-11-08 18:42:22 +0200

    created a validation function to check for any errors, still need to finish of the validation.

--- private/models/User.php
+++ private/models/User.php
@@ -3,4 +3,19 @@
 class User extends Model
 {
     protected $table = "USERS";
+
+    public function validate($data)
+    {
+        $this->errors = [];
+
+        if ($data['password'] != $data['retyped_password']) {
+            $this->errors['password'] = "Passwords do not match";
+        }
+
+        if (count($this->errors) == 0) {
+            return true;
+        }
+        
+        return false;
+    }
 }
