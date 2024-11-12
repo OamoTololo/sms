dd1c5d1b09ed49855a64da09a44fda7957a6fcd9
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-11-12 15:07:26 +0200

    removed fields that are going to be auto generated.

--- private/controllers/RegisterController.php
+++ private/controllers/RegisterController.php
@@ -9,15 +9,9 @@ class RegisterController extends Controller
             $user = new User;
 
             if ($user->validate($_POST)) {
-                $data['name'] = $_POST['name'];
-                $data['surname'] = $_POST['surname'];
-                $data['username'] = $_POST['username'];
-                $data['date'] = date('Y-m-d H:i:s');
-                $data['email'] = $_POST['email'];
-                $data['gender'] = $_POST['gender'];
-                $data['role'] = $_POST['role'];
+                $_POST['date'] = date('Y-m-d H:i:s');
 
-                $user->insert($data);
+                $user->insert($_POST);
                 $this->redirect('/LoginController');
             } else {
                 $errors = $user->errors;
