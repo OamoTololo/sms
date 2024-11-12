8799231d29611d65cb6ed47d5e3111d1d5bad550
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-11-12 15:11:54 +0200

    modified my code for better readbility.

--- private/views/auth/LoginView.php
+++ private/views/auth/LoginView.php
@@ -5,9 +5,7 @@ $this->view('includes/Header');
     <div class="container-fluid">
         <div class="p-5 mt-5 mx-auto border border-dark shadow rounded" style="width: 100%; max-width: 340px;">
             <h2 class="text-center">SA Schools</h2>
-            <img class="border border-primary d-block mx-auto img-fluid rounded-circle" style="width: 100px;"
-                 src="assets/img/SA.png"
-                 alt="Test Image">
+            <img class="border border-primary d-block mx-auto img-fluid rounded-circle" style="width: 100px;" src="assets/img/SA.png" alt="Test Image">
             <h3>Login</h3>
             <input class="form-control" type="email" name="email" placeholder="Email" autocomplete="off">
             <br>
