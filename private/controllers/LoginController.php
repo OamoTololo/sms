40bf4d8334a4394cd9881c59da194d495aea7fe0
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-11-12 15:06:24 +0200

    created a logic to check the email and password. If it's correct, it will redirect to the home page and if not, it will display the error message. created logic for the home controller to check if user is authenticated and redirects them accordingly.

--- private/controllers/LoginController.php
+++ private/controllers/LoginController.php
@@ -4,6 +4,23 @@ class LoginController extends Controller
 {
     public function index()
     {
-        $this->view('auth/LoginView', []);
+        $errors = [];
+
+        if (count($_POST) > 0) {
+            $user = new User;
+
+            if ($row = $user->where('email', $_POST['email'])) {
+                $row = $row[0];
+                if (password_verify($_POST['password'], $row->password)) {
+                    Auth::authenticate($row);
+                    $this->redirect('/HomeController');
+                }
+            }
+            $errors['email'] = "Wrong email or password";
+        }
+
+        $this->view('auth/LoginView', [
+            'errors' => $errors
+        ]);
     }
 }
