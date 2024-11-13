ec3c36b1a13964faa620d5877931cabfa4293d8a
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-11-13 18:30:14 +0200

    fixed the bug issue causing sessions to work. modified my code to be more cleaner.

--- private/models/Auth.php
+++ private/models/Auth.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 class Auth
 {
     public static function authenticate($row)
@@ -9,15 +9,27 @@ class Auth
 
     public static function loggedIn(): bool
     {
-        if (isset($_SESSION['USER'])) {
-            return true;
-        }
-        return false;
+        return isset($_SESSION['USER']);
     }
-    public static function logout($row)
+
+    public static function user()
+    {
+        return $_SESSION['USER']->name ?? false;
+    }
+    public static function logout()
     {
         if (isset($_SESSION['USER'])) {
             unset($_SESSION['USER']);
         }
     }
+    public static function __callStatic($method, $args)
+    {
+        $property = strtolower(str_replace('get', '', $method));
+
+        if (isset($_SESSION['USER']->{$property})) {
+            return $_SESSION['USER']->$property;
+        }
+        return 'Unknown';
+    }
+
 }
